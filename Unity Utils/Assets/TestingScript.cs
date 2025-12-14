14321cc308bbbc65161e2149287b53dc7fc5ff6a
mydimons <mydimons.kb@gmail.com>
2025-12-13 21:57:35 -0800

    Unity Package

--- Unity Utils/Assets/TestingScript.cs
+++ Unity Utils/Assets/TestingScript.cs
@@ -3,6 +3,7 @@ using UnityUtils.ScriptUtils;
 
 public class TestingScript : MonoBehaviour
 {
+    public AnimationCurve curve;
     public float testingValue;
     public Vector3 testingVector3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -10,7 +11,7 @@ public class TestingScript : MonoBehaviour
     {
         ObjectAnimations.AnimateTransformRotation(transform, new Vector3(transform.localRotation.x, transform.localRotation.y, transform.localRotation.z), new Vector3(4, 50, 90), 2);
         ObjectAnimations.AnimateTransformScale(transform, transform.localScale, new Vector3(3, 4, 5), 2);
-        ObjectAnimations.AnimateTransformPosition(transform, transform.position, new Vector3(0, 5, 0), 2);
+        ObjectAnimations.AnimateTransformPosition(transform, transform.position, new Vector3(0, 5, 0), 2, animationCurve: curve);
 
         ObjectAnimations.AnimateSpriteRendererOpacity(GetComponent<SpriteRenderer>(), 0.5f, 1, 2);
     }
